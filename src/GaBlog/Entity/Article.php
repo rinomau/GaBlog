269a0d02cc1ab79ec73a5d75b15148c31d5946f5
GianArb <gianarb92@gmail.com>
2013-01-08 23:41:31 +0100

    entity set :yellow_heart:

--- src/GaBlog/Entity/Article.php
+++ src/GaBlog/Entity/Article.php
@@ -7,13 +7,233 @@ use Doctrine\ORM\Mapping as ORM;
  */
 class Article
 {
+    const STATUS_Draft = 0;
+    const STATUS_Publish = 1;
+    const STATUS_Unpublish = 2;
+    const STATUS_Inactive = 3;
+    /**
+     * @ORM\Id @ORM\Column(type="integer") @ORM\GeneratedValue
+     * @var int
+     */
     private $id;
+    /**
+     * @ORM\Column(type="string")
+     * @var string
+     */
     private $title;
+    /**
+     * @ORM\Column(type="string")
+     * @var string
+     */
     private $description;
+    /**
+     * @ORM\Column(type="string")
+     * @var string
+     */
     private $content;
+    /**
+     * @ORM\Column(type="string")
+     * @var string
+     */
     private $tag;
-    private $dateTimeCreate;
+    /**
+     * @ORM\Column(type="datetime", name="created") @ORM\GeneratedValue
+     * @var datetime
+     */
+    private $dateTimeCreated;
+    /**
+     * @ORM\Column(type="datetime", name="publish") @ORM\GeneratedValue
+     * @var datetime
+     */
     private $dateTimePublish;
+    /**
+     * @ORM\Column(type="datetime", name="unpublish")
+     * @var datetime
+     */
     private $dateTimeUnpublish;
+    /**
+     * @ORM\Column(type="datetime")
+     * @var int
+     */
+    private $status;
+    /**
+     * @ORM\Column(type="integer")
+     * @var int
+     */
     private $idUser;
+
+    /**
+     * @return int
+     */
+    function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string
+     */
+    function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * @return string
+     */
+    function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * @return string
+     */
+    function getContent()
+    {
+        return $this->content;
+    }
+
+    /**
+     * @return string
+     */
+    function getTag()
+    {
+        return $this->tag;
+    }
+
+    /**
+     * @return datetime
+     */
+    function getDateTimeCreated()
+    {
+        return $this->dateTimeCreated;
+    }
+
+    /**
+     * @return datetime
+     */
+    function getDateTimePublish()
+    {
+        return $this->dateTimePublish;
+    }
+
+    /**
+     * @return datetime
+     */
+    function getDateTimeUnpublish()
+    {
+        return $this->dateTimeUnpublish;
+    }
+
+    /**
+     * @return int
+     */
+    function getStatus()
+    {
+        return $this->status;
+    }
+
+    /**
+     * @return int
+     */
+    function getIdUser()
+    {
+        return $this->idUser;
+    }
+    /**
+     * @param $id
+     * @return $this
+     */
+    function setId($id)
+    {
+        $this->id = $id;
+        return $this;
+    }
+
+    /**
+     * @param $t
+     * @return $this
+     */
+    function setTitle($t)
+    {
+        $this->title = $t;
+        return $this;
+    }
+
+    /**
+     * @param $d
+     * @return $this
+     */
+    function setDescription($d)
+    {
+        $this->description = $d;
+        return $this;
+    }
+
+    /**
+     * @param $c
+     * @return $this
+     */
+    function setContent($c)
+    {
+        $this->content = $c;
+        return $this;
+    }
+
+    /**
+     * @param $t
+     * @return $this
+     */
+    function setTag($t)
+    {
+        $this->tag = $t;
+        return $this;
+    }
+
+    /**
+     * @param $dtc
+     * @return $this
+     */
+    function setDateTimeCreated($dtc)
+    {
+        $this->dateTimeCreated = $dtc;
+        return $this;
+    }
+
+    /**
+     * @param $dtp
+     * @return $this
+     */
+    function setDateTimePublish($dtp)
+    {
+        $this->dateTimePublish = $dtp;
+        return $this;
+    }
+
+    /**
+     * @param $dtu
+     * @return $this
+     */
+    function setDateTimeUnpublish($dtu)
+    {
+        $this->dateTimeUnpublish = $dtu;
+        return $this;
+    }
+
+    /**
+     * @param int $s
+     * @return $this
+     */
+    function setStatus($s = 0)
+    {
+        $this->status = $s;
+        return $this;
+    }
+
+    function setIdUser($i)
+    {
+        $this->idUser = $i;
+        return $this;
+    }
 }
--- src/GaBlog/Entity/Category.php
+++ src/GaBlog/Entity/Category.php
@@ -7,10 +7,142 @@ use Doctrine\ORM\Mapping as ORM;
  */
 class Category
 {
+    /**
+     * @ORM\Id @ORM\Column(type="integer") @ORM\GeneratedValue
+     * @var int
+     */
     private $id;
+    /**
+     * @ORM\Column(type="string")
+     * @var string
+     */
     private $name;
+    /**
+     * @ORM\Column(type="string")
+     * @var string
+     */
     private $description;
+    /**
+     * @ORM\Column(type="string")
+     * @var string
+     */
     private $tag;
-    private $dateTimeCreate;
+    /**
+     * @ORM\Column(type="datetime", name="created") @ORM\GeneratedValue
+     * @var datetime
+     */
+    private $dateTimeCreated;
+    /**
+     * @ORM\Column(type="integer")
+     * @var int
+     */
     private $idUser;
+
+    /**
+     * @return int
+     */
+    function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string
+     */
+    function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @return string
+     */
+    function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * @return string
+     */
+    function getTag()
+    {
+        return $this->tag;
+    }
+
+    /**
+     * @return datetime
+     */
+    function getDateTimeCreated()
+    {
+        return $this->dateTimeCreated;
+    }
+
+    /**
+     * @return int
+     */
+    function getIdUser()
+    {
+        return $this->idUser;
+    }
+
+    /**
+     * @param $i
+     * @return $this
+     */
+    function setId($i)
+    {
+        $this->id = $i;
+        return $this;
+    }
+
+    /**
+     * @param $n
+     * @return $this
+     */
+    function setName($n)
+    {
+        $this->name = $n;
+        return $this;
+    }
+
+    /**
+     * @param $d
+     * @return $this
+     */
+    function setDescription($d)
+    {
+        $this->description = $d;
+        return $this;
+    }
+
+    /**
+     * @param $t
+     * @return $this
+     */
+    function setTag($t)
+    {
+        $this->tag = $t;
+        return $this;
+    }
+
+    /**
+     * @param $dtc
+     * @return $this
+     */
+    function setDateTimeCreated($dtc)
+    {
+        $this->dateTimeCreated = $dtc;
+        return $this;
+    }
+
+    /**
+     * @param $iu
+     * @return $this
+     */
+    function setIdUser($iu)
+    {
+        $this->idUser = $iu;
+        return $this;
+    }
 }
