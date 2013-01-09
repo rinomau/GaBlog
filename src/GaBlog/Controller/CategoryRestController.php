2f49bbe30c75854dba4468ed2b495f814c7d43eb
GianArb <gianarb92@gmail.com>
2013-01-09 20:17:55 +0100

    check controller Rest

--- src/GaBlog/Controller/CategoryRestController.php
+++ src/GaBlog/Controller/CategoryRestController.php
@@ -1,3 +1,36 @@
 <?php
 namespace GaBlog\Controller;
+use Zend\Mvc\Controller\AbstractRestfulController;
+use Zend\View\Model\ViewModel;
 
+class CategoryRestController
+    extends AbstractRestfulController
+{
+    public function getList()
+    {
+        $c = array('c' => 'b', 'd' => 'e');
+        $view = new ViewModel($c);
+        $view->setTerminal(true);
+        return $view;
+    }
+
+    public function get($id)
+    {
+
+    }
+
+    public function create($data)
+    {
+
+    }
+
+    public function update($id, $data)
+    {
+
+    }
+
+    public function delete($id)
+    {
+
+    }
+}
